0cae302401aa00bee443675bde46cec60beb7cc4
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2023-09-16 11:06:48 -0600

    move information section to informationSection component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,3 +1,4 @@
+import InformationSection from "../components/InformationSection"
 import InstructionSection from "../components/InstructionSection"
 
 export default function App() {
@@ -45,52 +46,7 @@ export default function App() {
             <div className="col-span-1"></div>
           </form>
         </section>
-        <section className="flex gap-x-2">
-          {/* variables */}
-          <div className="flex-1 flex flex-col border">
-            <h2 className="text-center border">Datos</h2>
-
-            <div className="flex-1 border">
-              <ul className="grid grid-cols-3">
-                <li className="p-2 border border-collapse">
-                  Adreess
-                </li>
-                <li className="p-2 border border-collapse">
-                  Var
-                </li>
-                <li className="p-2 border border-collapse">
-                  Value
-                </li>
-
-                <li className="p-2 border border-collapse">0000</li>
-                <li className="p-2 border border-collapse">A</li>
-                <li className="p-2 border border-collapse">4</li>
-              </ul>
-            </div>
-            <div className="flex flex-col">
-              <button className="border">Add variable</button>
-            </div>
-          </div>
-          {/* Stack */}
-          <div className="flex-1 border">
-            <h2 className="text-center border">Pila</h2>
-            <ul className="grid grid-cols-3">
-              <li className="p-2 border border-collapse">
-                Adreess
-              </li>
-              <li className="p-2 border border-collapse">
-                Var
-              </li>
-              <li className="p-2 border border-collapse">
-                Value
-              </li>
-
-              <li className="p-2 border border-collapse">0000</li>
-              <li className="p-2 border border-collapse">A</li>
-              <li className="p-2 border border-collapse">4</li>
-            </ul>
-          </div>
-        </section>
+        <InformationSection />
       </div>
 
     </main>
