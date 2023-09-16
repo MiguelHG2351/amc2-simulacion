b70d18e562454128ae559cfa577f55adb168b98b
MiguelHG2351 <miguelhernandezgaitan13@gmail.com>
2023-09-16 10:59:55 -0600

    add variable to array of instruction

--- src/components/Modal/InstructionModal.tsx
+++ src/components/Modal/InstructionModal.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react'
 import { useStore } from '@nanostores/react';
-import { $operationList } from '../../store';
+import { $operationList, $variableList } from '../../store';
+import type { OperationCodeInstruction } from '../../store';
+import { useForm } from 'react-hook-form'
 
 type InstructionModalProps = {
   isOpen: boolean;
@@ -10,20 +12,29 @@ type InstructionModalProps = {
 
 export const InstructionModal: React.FC<InstructionModalProps> = ({ setIsOpen }) => {
   const operationList = useStore($operationList);
+  const variableList = useStore($variableList);
   console.log(operationList);
+
+  const { handleSubmit, register, formState: { errors }, reset } = useForm();
+
+  const onSubmit = (data: any) => {
+    console.log(data, errors)
+    if(errors.name || errors.variable) return console.log(errors)
+    setIsOpen(false)
+  };
   
   return (
-    <form className="bg-white px-4 py-3 sm:px-6">
+    <form className="bg-white px-4 py-3 sm:px-6" onSubmit={handleSubmit(onSubmit)}>
       <div className="flex gap-x-4">
         <label className='block flex-1'>
           <span className="block text-sm font-medium text-gray-700">
             Instruction
           </span>
-          <select name="name" className='w-full'>
+          <select {...register('name', {required: true})} className='w-full'>
             <option value="PUSH">PUSH</option>
             {
               Object.keys(operationList).map((value, index) => (
-                <option value={value} key={index}>{value}</option>
+                <option value={value} key={operationList[value as OperationCodeInstruction]!}>{value}</option>
               ))
             }
           </select>
@@ -32,27 +43,34 @@ export const InstructionModal: React.FC<InstructionModalProps> = ({ setIsOpen })
           <span className="block text-sm font-medium text-gray-700">
             Variable
           </span>
-          <select name="variable" className='w-full'>
-            <option value="PUSH">A</option>
-            <option value="POP">B</option>
+          <select {...register('variable', {required: true})} className='w-full'>
+            {variableList.map(variable => (
+              <option value="PUSH">A</option>
+            ))}
           </select>
         </label>
+      </div>
+      <div className="error">
+        {(errors.name || errors.variable) && (
+          <span className='text-red-600 font-bold'>
+            There fields is required: {errors.name && 'name'} {errors.variable && 'variable'}
+          </span>
+        )}
       </div>
       <div className="bg-gray-50 py-3 sm:flex sm:flex-row-reverse">
         <button
           type="submit"
           tabIndex={0}
           className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-green-600 text-base font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:ml-3 sm:w-auto sm:text-sm"
-          onClick={() => setIsOpen(false)}
-        >
+          >
           Add
         </button>
         <button
           type="button"
           tabIndex={0}
           className="mt-3 w-full inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:mt-0 sm:ml-3 sm:w-auto sm:text-sm"
-          onClick={() => setIsOpen(false)}
-        >
+          onClick={() => {reset();setIsOpen(false);}}
+          >
           Cancel
         </button>
       </div>
--- src/store/index.ts
+++ src/store/index.ts
@@ -3,19 +3,20 @@ import { atom } from "nanostores"
 import { operationCodes } from '../utils/constant'
 
 export type OperationCode = typeof operationCodes
-
 export const $operationList = atom< OperationCode >(operationCodes)
+export const $variableList = atom< string[] >([])
 
 
-
-type OperationCodeValues = typeof operationCodes[keyof typeof operationCodes];
-type OperationCodeInstruction = keyof typeof operationCodes;
+export type OperationCodeValues = typeof operationCodes[keyof typeof operationCodes];
+export type OperationCodeInstruction = keyof typeof operationCodes;
 type IInstruction = {
   address: string,
+  variable: string,
   instruction: OperationCodeInstruction,
   instructionCode: OperationCodeValues,
 }
 export const $instructionList = atom< IInstruction[] >([])
+
 
 export const addInstruction = (instruction: IInstruction) => {
   $instructionList.set([...$instructionList.get(), instruction])
